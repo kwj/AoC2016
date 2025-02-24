d36dc50b272b7ba9167188e9e2f6130d72baf7e0
kwj <kwj@vlax.net>
2025-02-24 12:14:12 -0800

    Tweak

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,8 @@
 package net.vlax.aoc2016
 
 @main def repl(): Unit =
-  import scala.collection.Iterator
   import scala.io.StdIn
-  import net.vlax.aoc2016.sol.{Solution, Solver}
+  import net.vlax.aoc2016.sol.Solver
 
   println("Please enter a date number, or Ctrl-D to exit.")
   Iterator
