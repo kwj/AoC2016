fbf8d26e76c78c0e44a5f3cd96e19aecbc2ca313
kwj <kwj@vlax.net>
2025-02-26 16:57:13 -0800

    Improve readability

--- src/main/scala/sol/Day08.scala
+++ src/main/scala/sol/Day08.scala
@@ -30,7 +30,7 @@ class Day08(src: BufferedSource) extends Solution:
   private class Grid(val nRow: Int, val nCol: Int):
     private val grid = Array.fill(nRow)(Array.fill(nCol)('.'))
 
-    def op(inst: Rect): Unit =
+    def opRect(inst: Rect): Unit =
       for
         r <- 0 until inst.nRow
         c <- 0 until inst.nCol
@@ -40,41 +40,39 @@ class Day08(src: BufferedSource) extends Solution:
       val len = seq.size
       seq.drop(len - (n % len)) ++ seq.take(len - (n % len))
 
-    def op(inst: RotateRow): Unit =
+    def opRow(inst: RotateRow): Unit =
       val newSeq = circleShiftRight(grid(inst.idx), inst.n)
       newSeq.copyToArray(grid(inst.idx))
 
-    def op(inst: RotateCol): Unit =
+    def opCol(inst: RotateCol): Unit =
       val newSeq = circleShiftRight(grid.map(_(inst.idx)), inst.n)
       for r <- 0 until nRow do grid(r)(inst.idx) = newSeq(r)
 
-    def countUp(): Int = grid.map(_.count(_ == '#')).sum
+    def nLights: Int = grid.map(_.count(_ == '#')).sum
 
-    def display(): String = grid.map(_.mkString).mkString("\n")
+    def display: String = grid.map(_.mkString).mkString("\n")
 
   private inline val Height = 6
   private inline val Width = 50
 
-  private def execOperations(): Grid =
+  private def swipeCard(insts: Seq[Instruction]): Grid =
     val g = Grid(Height, Width)
 
-    instructions.foreach(inst =>
-      inst match
-        case Rect(_, _) => g.op(inst.asInstanceOf[Rect])
-        case RotateRow(_, _) => g.op(inst.asInstanceOf[RotateRow])
-        case RotateCol(_, _) => g.op(inst.asInstanceOf[RotateCol])
-    )
+    insts.foreach({
+      case inst: Rect => g.opRect(inst)
+      case inst: RotateRow => g.opRow(inst)
+      case inst: RotateCol => g.opCol(inst)
+    })
+
     g
 
-  def partOne(): String =
-    val screen = execOperations()
+  private lazy val screen = swipeCard(instructions)
 
-    "%d".format(screen.countUp())
+  def partOne(): String =
+    "%d".format(screen.nLights)
 
   def partTwo(): String =
-    val screen = execOperations()
-
-    screen.display()
+    screen.display
 
   def solve(): Unit =
     printf("%s\n", partOne())
